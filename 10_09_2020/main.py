0d458817030946c19fb3ae5ac4a19b3d3999dafa
Valery Petrov <petrov@technokratos.com>
2020-09-10 12:53:02 +0300

    Read data from csv

--- 10_09_2020/main.py
+++ 10_09_2020/main.py
@@ -1,3 +1,14 @@
-# Press the green button in the gutter to run the script.
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def read_csv(file):
+    data = pd.read_csv(file, usecols=['gender', 'age'])
+    return data
+
+
 if __name__ == '__main__':
-    print('hi')
+    data = read_csv(open('data.csv'))
+    print(data['gender'])
+    print(data['age'])
