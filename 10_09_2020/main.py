7ff3460236b3a6e3152085262ebf7588306f6ab7
Valery Petrov <petrov@technokratos.com>
2020-09-10 13:03:32 +0300

    Prettify code

--- 10_09_2020/main.py
+++ 10_09_2020/main.py
@@ -3,12 +3,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def read_csv(file):
-    data = pd.read_csv(file, usecols=['Sex', 'Age'])
-    return data
+def read_data(file):
+    return pd.read_csv(file, usecols=['Sex', 'Age'])
 
 
 if __name__ == '__main__':
-    data = read_csv(open('data.csv'))
+    data = read_data(open('titanic.csv'))
     print(data['Sex'])
     print(data['Age'])
