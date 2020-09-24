6f2b389fdfdc76c54d78d91ad591a8ee46b71cc0
Valery Petrov <petrov@technokratos.com>
2020-09-24 12:40:37 +0300

    Calculate circle center

--- 24_09_2020/main.py
+++ 24_09_2020/main.py
@@ -4,4 +4,7 @@ if __name__ == '__main__':
     n, k = 100, 4
     x = np.random.randint(1, 100, n)
     y = np.random.randint(1, 100, n)
+    x_cc = np.mean(x)
+    y_cc = np.mean(y)
     print(x)
+    print(x_cc)
